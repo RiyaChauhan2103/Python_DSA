3a3618a71d1d485475c15ca98deb618b5cd2742e
Riya Chauhan <146920888+RiyaChauhan2103@users.noreply.github.com>
2025-10-22 13:47:27 +0200

    Time: 0 ms (100.00%) | Memory: 18.7 MB (26.77%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,19 +7,30 @@ class Solution:
         #         return i
          
         # return len(nums)
-        if target> nums[-1]:
-            return len(nums)
-        if target < nums[0]:
-            return 0
-        low ,high = 0, len(nums) - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] == target: 
-                return mid
-            if nums[mid] < target: 
-                low = mid + 1
-                if low < len(nums) and nums[low]>target:
-                    return low
-            else: 
-                high = mid - 1
+        # if target> nums[-1]:
+        #     return len(nums)
+        # if target < nums[0]:  
+        #     return 0
+        # low ,high = 0, len(nums) - 1
+        # while low <= high:
+        #     mid = (low + high) // 2
+        #     if nums[mid] == target: 
+        #         return mid
+        #     if nums[mid] < target: 
+        #         low = mid + 1
+        #         if low < len(nums) and nums[low]>target:
+        #             return low
+        #     else: 
+        #         high = mid - 1
+        ##### ============ alternate=========####
+        low,high=0,len(nums)-1
+        lb=len(nums)
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>=target:
+                lb=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return lb
 
