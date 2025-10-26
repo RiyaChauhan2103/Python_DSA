e3cbea204e67e8690047583eaf98689b438d9d97
Riya Chauhan <146920888+RiyaChauhan2103@users.noreply.github.com>
2025-10-26 15:52:30 +0100

    Time: 2 ms (65.42%) | Memory: 17.7 MB (51.13%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,16 +1,28 @@
 class Solution:
+    # def mySqrt(self, x: int) -> int:
+    #     if x==1: return 1
+    #     min_val=1;
+    #     max_val=x//2;
+    #     for i in range(1,(x//2)+1):
+    #         # print(i)
+    #         if i*i==x:
+    #             return i
+    #         elif i*i<x:
+    #             min_val=i;
+    #         elif i*i>x:
+    #             # print(max_value)
+    #             max_val=i
+    #             break;
+    #     return (min_val+max_val)//2  
+
     def mySqrt(self, x: int) -> int:
-        if x==1: return 1
-        min_val=1;
-        max_val=x//2;
-        for i in range(1,(x//2)+1):
-            # print(i)
-            if i*i==x:
-                return i
-            elif i*i<x:
-                min_val=i;
-            elif i*i>x:
-                # print(max_value)
-                max_val=i
-                break;
-        return (min_val+max_val)//2    
+        left,right=1,x
+        while left<=right:
+            mid=left+(right-left)//2
+            if mid*mid==x:
+                return mid
+            elif mid*mid>x:
+                right=mid-1
+            else:
+                left=mid+1
+        return right
